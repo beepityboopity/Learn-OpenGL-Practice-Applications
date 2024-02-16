9d45ef70760f67074d01df5fd1dc0f36c61ad9b1
beepityboopity <143839769+beepityboopity@users.noreply.github.com>
2024-02-16 15:42:55 -0500

    added score and constant meat spawns

--- Getting Started/Triangle Collects Triangles/Triangle Collects Triangles/main.cpp
+++ Getting Started/Triangle Collects Triangles/Triangle Collects Triangles/main.cpp
@@ -4,6 +4,9 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
 // https://www.topcoder.com/thrive/articles/Geometry Concepts part 2:%20 Line Intersection and its Applications
 
@@ -22,44 +25,54 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vec4(0.0f, 0.2f, 0.9f, 1.0f);\n"
 "}\n\0";
 
 const char* fragmentShaderSource1 = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.0f, 0.5f, 0.6f, 1.0f);\n"
+"   FragColor = vec4(0.9f, 0.0f, 0.1f, 1.0f);\n"
 "}\n\0";
 
-// timing
-float deltaTime = 0.0f;	// time between current frame and last frame
+float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 float player[] = {
-        -0.1f, -0.1f, 0.0f, // 0 1 2
-        0.1f, -0.1f, 0.0f,  // 3 4 5
-        0.0f, 0.1f, 0.0f,   // 6 7 8
+        -0.1f, -0.1f, 0.0f,
+        0.1f, -0.1f, 0.0f,
+        0.0f, 0.1f, 0.0f,
 };
 
 float meats[] = {
-        -0.9f, -0.9f, 0.0f, // 0 1 2
-        -0.7f, -0.9f, 0.0f, // 3 4 5
-        -0.8f, -0.7f, 0.0f, // 6 7 8
+        -0.9f, -0.9f, 0.0f,
+        -0.7f, -0.9f, 0.0f,
+        -0.8f, -0.7f, 0.0f,
 };
 
-int main() {
+int score = 0;
 
+int main() {
+    srand(static_cast <unsigned> (time(0)));
     glfwInit();
+    const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Triangle Collects Triangles", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Triangle Cannibalism", NULL, NULL);
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    ImGui::StyleColorsClassic();
 
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     unsigned int fragmentOne = glCreateShader(GL_FRAGMENT_SHADER);
@@ -94,14 +107,28 @@ int main() {
         lastFrame = currentFrame;
 
         processInput(window, player);
-        collisionCheck(player, meats);
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
+        if (collisionCheck(player, meats)) {
+            float rx = -0.9f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (0.9f - (-0.9f))));
+            float ry = -0.9f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (0.9f - (-0.9f))));
+            meats[0] = rx - 0.1;
+            meats[1] = ry - 0.1;
+            meats[3] = rx + 0.1;
+            meats[4] = ry - 0.1;
+            meats[6] = rx;
+            meats[7] = ry + 0.1; 
+            score += 1;
+        }
 
+        ImGui::Begin("Cannibalized Triangles");
+        ImGui::Text(std::to_string(score).c_str());
+        ImGui::End();
 
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-
 
         glBindVertexArray(VAOs[0]);
         glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
@@ -121,9 +148,16 @@ int main() {
         glBindVertexArray(VAOs[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
 	return 0;
 }
 
@@ -168,7 +202,6 @@ bool calculateCollision(float x1, float x2, float y1, float y2, float x3, float 
     float x = (B2 * C1 - B1 * C2) / det;
     float y = (A1 * C2 - A2 * C1) / det;
     
-
     if (std::min(x1, x2) <= x && x <= std::max(x1, x2) && 
         std::min(x3, x4) <= x && x <= std::max(x3, x4) &&
         std::min(y1, y2) <= y && y <= std::max(y1, y2) &&
@@ -200,16 +233,13 @@ bool collisionCheck(float triangle1[], float triangle2[]) {
         triangle2[0], triangle2[6], triangle2[1], triangle2[7]);
     
     if ((check1 || check2) || (check3 || check4) || (check5 || check6)) {
-        std::cout << "true" << std::endl;
         return true;
     }
     else {
-        std::cout << "false" << std::endl;
         return false;
     }
 }
 
-// whenever the window size changes this callback function executes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
